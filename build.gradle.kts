d09a327439579c829ebf85dc53d13bc9c6037cf8
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2026-01-21 11:09:15 +0200

    Build: add signing/publish configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,4 +47,12 @@ tasks {
         sourceCompatibility = "21"
         targetCompatibility = "21"
     }
+    signPlugin {
+        certificateChain.set(providers.environmentVariable("CERTIFICATE_CHAIN"))
+        privateKey.set(providers.environmentVariable("PRIVATE_KEY"))
+        password.set(providers.environmentVariable("PRIVATE_KEY_PASSWORD"))
+    }
+    publishPlugin {
+        token.set(System.getenv("JETBRAIN_TOKEN"))
+    }
 }
